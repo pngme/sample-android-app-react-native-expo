2f82b3d2afd6c8c62cac3a3bf729566130636f6d
Aditya Narendra Rao <13aditya13@gmail.com>
2022-10-26 14:08:29 -0500

    Updating package.json to reference v3.0.0 of rn-sms-pngme-android pkg

--- src/screens/permissions/index.js
+++ src/screens/permissions/index.js
@@ -1,4 +1,4 @@
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Button, NativeModules, PermissionsAndroid, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import { go } from '@pngme/react-native-sms-pngme-android';
 import Checkbox from 'expo-checkbox';
@@ -15,8 +15,29 @@ const Permissions = ({ navigation }) => {
     navigation.navigate('LoanApplication');
   };
 
-  const handleContinue = async () => {
-    if (toggleCheckBox) {
+  // Here we check if permission can be asked again, if user clicks on `never ask again` it will return false
+  const canPermissionBeAskedAgain = async() => {
+    try {
+      const canIt = await NativeModules.PermissionsAndroid.shouldShowRequestPermissionRationale(PermissionsAndroid.PERMISSIONS.READ_SMS)
+      return canIt;
+    } catch(e) {
+      return true
+    }
+  }
+
+  const requestSMSPermission = async () => {
+    try {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_SMS
+      );
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const handleContinue = async() => {
+    if (toggleCheckBox && !user.pngmePermissionWasSelected) {
+      // if user confirm they want to use Pngme, we store that selection
       setUser({ pngmePermissionWasSelected: true });
       await go({
         clientKey: Constants.manifest.extra.pngmeSdkToken,
@@ -27,12 +48,16 @@ const Permissions = ({ navigation }) => {
         isKycVerified: false,
         companyName: 'Acme Bank',
         externalId: '',
+        hidePngmeDialog: false, // if you want to hide the Pngme dialog, set this to true
       });
-      navigateToLoanScreen();
-    } else {
-      navigateToLoanScreen();
+    } else if (toggleCheckBox && user.pngmePermissionWasSelected) {
+      const reRequestSMSPermission = await canPermissionBeAskedAgain();
+      if (reRequestSMSPermission) {
+        await requestSMSPermission();
+      }
     }
-  };
+    navigateToLoanScreen();
+  }
 
   const renderCheckboxLine = (label, isSelected, onValueChange, disabled = false) => (
     <View style={styles.checkboxContainer}>
